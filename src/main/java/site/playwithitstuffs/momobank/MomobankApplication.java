9d9623cf36d2fb2a5fb3eceba6e855015e501997
Manoj Pal <man-oz@Manojs-MacBook-Air.local>
2025-07-06 12:03:59 +0530

    added files and source code

---
+++ src/main/java/site/playwithitstuffs/momobank/MomobankApplication.java
@@ -0,0 +1,13 @@
+package site.playwithitstuffs.momobank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MomobankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MomobankApplication.class, args);
+	}
+
+}
